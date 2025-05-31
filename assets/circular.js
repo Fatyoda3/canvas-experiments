4a5a71804a656197cf6f13fe91d93b40f62a55c9
FatYoda <shivangsingh654@gmail.com>
2025-05-31 20:19:02 +0530

    hello I am fixing my old stuff, now I am working on Circular and  it's much better

--- assets/circular.js
+++ assets/circular.js
@@ -1,275 +1,105 @@
-let share = 10;
-
-
-class Line {
-    constructor(x, y, effect, c) {
-
-        this.x = x;
-        this.y = y;
-
-        this.radius = Math.floor(Math.random() * 3) + 2;
-        this.diameter = this.radius * 2;
+class Particle {
+    constructor(effect, context, spawnAtMouse = false) {
         this.effect = effect;
-
-
-        this.c = c;
-
-        // this.c.fillStyle = 'white';
-
-        // this.x = offset * Math.random() + this.effect.width / 2;
-
-        // this.y = offset * Math.random() + this.effect.height / 2;
-
-        this.color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 127)},${Math.floor(Math.random() * 127)})`;
-        this.radians = Math.random() * Math.PI * 2;
-
-        // this.radians = 50 * Math.random();
-
-        this.V = 0.01;
-
-        //range function ( Max - Min + 1 )
-        this.factor = 50 + share;
-        // this.factor = (150 * Math.random()) + 52;
-        // this.factor = 100 * Math.random() + 50;
-        // this.offset = Math.floor(Math.random() * this.diameter * 2);
-        share += 5;
-        console.log(share);
+        this.context = context;
+        this.spawnAtMouse = spawnAtMouse;
+        this.reset();
     }
-
-
-    strokeCircle(lastPoint) {
-
-        this.c.fillStyle = this.color;
-
-        this.c.lineWidth = this.width;
-
-
-        this.c.beginPath();
-
-        this.c.arc(this.x, this.y, this.radius, 0, 360)
-
-        this.c.fill();
-
-        this.c.closePath();
-
-        /* 
-                this.c.beginPath();
-        
-                this.c.arc(this.x + 50, this.y + 100, this.radius, 0, 360);
-                this.c.fill();
-        
-                this.c.closePath();
-        
-                this.c.beginPath();
-        
-                this.c.arc(this.x + 150, this.y + 100, this.radius, 0, 360);
-                this.c.fill();
-                this.c.closePath();
-        
-                this.c.beginPath();
-        
-                this.c.arc(this.x + 250, this.y + 100, this.radius, 0, 360)
-         */
-        // this.c.arc(this.x + 50, this.y+100, this.radius, 0, 360)
-        // this.c.arc(this.x + 150, this.y+100, this.radius, 0, 360)
-        // this.c.arc(this.x + 150, this.y+100, this.radius, 0, 360) create a cool triangle
-
-
-        this.c.fill();
-        this.c.closePath();
-        /* 
-                this.c.beginPath();
-                this.c.fillStyle = this.color;
-                this.c.lineWidth = 15
-                this.c.moveTo(lastPoint.x, lastPoint.y);
-                this.c.lineTo(this.x, this.y);
-                this.c.stroke();
-                this.c.closePath(); */
-
-
-    }
-
-
-    update(x, y) {
-        const lastPoint = { x: this.x, y: this.y };
-
-        this.radians += this.V;
-
-        if (this.radians > 360 && this.radians < 1)
-            this.radians = - this.radians;
-
-
-        // const x1 = Math.cos(this.radians);
-        // const y2 = Math.sin(this.radians);
-
-        this.x = this.effect.mouse.x + Math.floor(Math.cos(this.radians) * this.factor);
-
-        this.y = this.effect.mouse.y + Math.floor(Math.sin(this.radians) * this.factor);
-
-        // this.x = x + Math.floor(Math.cos(this.radians) * this.factor);
-        // this.y = y + Math.floor(Math.sin(this.radians) * this.factor);
-
-        /*        if (this.factor > 450)//this was the bug
-                   this.factor = -this.factor;
-       
-               this.factor++; */
-
-
-
-
-
-
-        this.strokeCircle(lastPoint);
-    }
-
 
     reset() {
+        this.alpha = 1;
+        this.hue = Math.random() * 360;
+        this.radius = Math.random() * 3 + 2;
+        this.radians = Math.random() * Math.PI * 2;
+        this.velocity = 0.01 + Math.random() * 0.02;
+        this.distance = 30 + Math.random() * 120;
+        this.gravity = 0.1 + Math.random() * 0.3;
 
-        this.x = this.effect.width / 2 - (this.diameter);
+        if (this.spawnAtMouse) {
+            this.originX = this.effect.mouse.x;
+            this.originY = this.effect.mouse.y;
+        } else {
+            this.originX = this.effect.canvas.width / 2;
+            this.originY = this.effect.canvas.height / 2;
+        }
 
-        this.y = this.effect.height / 2 - (this.diameter);
+        this.yOffset = 0;
+    }
 
+    update() {
+        this.hue += 1;
+        this.radians += this.velocity;
+        this.alpha -= 0.003; // fade out
+        if (this.alpha <= 0) {
+            this.reset();
+        }
+
+        this.yOffset += this.gravity;
+
+        this.x = this.originX + Math.cos(this.radians) * this.distance;
+        this.y = this.originY + Math.sin(this.radians) * this.distance + this.yOffset;
+
+        this.draw();
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.fillStyle = `hsla(${this.hue}, 100%, 70%, ${this.alpha})`;
+        this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        this.context.fill();
+        this.context.closePath();
     }
 }
-
 
 class Effect {
-
     constructor(canvas, context) {
-
-        this.width = canvas.width;
-        this.height = canvas.height;
-
-        this.LineCount = 100;
-        this.LineArray = [];
-
         this.canvas = canvas;
         this.context = context;
+        this.particles = [];
+        this.mouse = { x: canvas.width / 2, y: canvas.height / 2 };
+        this.resize();
+        this.init();
+        this.addEvents();
+    }
 
-        this.generateParticles();
-        this.mouse = { x: this.canvas.width / 2, y: this.canvas.height / 2, pressed: false };
+    init() {
+        this.particles = [];
+        for (let i = 0; i < 100; i++) {
+            this.particles.push(new Particle(this, this.context));
+        }
+    }
 
-
-        window.addEventListener('resize', () => {
-            this.resize(window.innerWidth, window.innerHeight);
-
-        })
-
-
-        window.addEventListener('mousedown', (e) => {
-            this.mouse.pressed = true;
+    addEvents() {
+        window.addEventListener('resize', () => this.resize());
+        window.addEventListener('mousemove', e => {
             this.mouse.x = e.clientX;
             this.mouse.y = e.clientY;
-        })
-
-        window.addEventListener('mousemove', (e) => {
-            if (this.mouse.pressed) {
-                this.mouse.x = e.clientX;
-                this.mouse.y = e.clientY;
-            }
-        })
-        window.addEventListener('mouseup', () => {
-            this.mouse.pressed = false;
-
-        })
-
-
+        });
+        window.addEventListener('click', () => this.spawnMore());
     }
 
-    generateParticles() {
-
-
-        for (let i = 0; i < this.LineCount; i++)
-            this.LineArray[i] = (new Line(this.canvas.width / 2, this.canvas.height / 2, this, this.context));
-
-    }
-
-    handleLines() {
-        for (let i = 0; i < this.LineCount; i++) {
-
-            this.LineArray[i].update(this.canvas.width / 2, this.canvas.height / 2);
-            /*        this.LineArray[i].update(this.canvas.width / 3, this.canvas.height / 3);
-                   this.LineArray[i].update(this.canvas.width / 4, this.canvas.height / 4);
-                   this.LineArray[i].update(this.canvas.width / 5, this.canvas.height / 5); */
-            /*       this.LineArray[i].update(this.canvas.width / 8, this.canvas.height / 2);
-                  this.LineArray[i].update(Math.floor(this.canvas.width/1.1), this.canvas.height / 2); */
-
+    spawnMore() {
+        for (let i = 0; i < 10; i++) {
+            this.particles.push(new Particle(this, this.context, true));
         }
-
-    }
-    resize(w, h) {
-        // resetFILL();
-        this.width = w;
-        this.height = h;
-
-        this.canvas.width = w;
-        this.canvas.height = h;
-
-        this.LineArray.forEach(p => {
-
-            p.reset();
-
-        })
-
     }
 
+    resize() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
 
-
+    animate() {
+        this.context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.particles.forEach(p => p.update());
+        requestAnimationFrame(() => this.animate());
+    }
 }
 
-
-
-
 window.addEventListener('load', () => {
-
-
-    const canvas = document.querySelector('canvas');
+    const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
-
-    context.fillStyle = "white";
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-
-    const screen = new Effect(canvas, context);
-
-    /*     function resetFILL() {
-            context.fillStyle = "white";
-    
-        }
-     */
-
-
-
-
-    const animate = () => {
-
-        // context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = 'rgb(0,0,0,0.05)';
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        (() => {
-            context.save();
-            context.strokeStyle = 'white';
-            context.moveTo(canvas.width / 2, 0);
-            context.lineTo(canvas.width / 2, canvas.height);
-
-            context.moveTo(0, canvas.height / 2);
-            context.lineTo(canvas.width, canvas.height / 2);
-            context.stroke();
-
-            context.restore();
-
-        })()
-
-        screen.handleLines();
-
-
-        requestAnimationFrame(animate);
-
-    }
-
-    animate();
-
-})
-
+    const effect = new Effect(canvas, context);
+    effect.animate();
+});
